af322fdf76fd16c51b2133a1db38bfc225853c3e
Flash-kir <flash-kir@yandex.ru>
2023-03-16 13:15:16 +0300

    send logs to Telegram to admin user

--- devman_bot.py
+++ devman_bot.py
@@ -7,22 +7,21 @@ import telegram
 from dotenv import load_dotenv
 from time import sleep, time
 
+
+class MyLogsHandler(logging.Handler):
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        bot.send_message(
+            chat_id=args.user_id,
+            text=f'{log_entry}'
+        )
+
+
 logger = logging.getLogger('bot')
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-id',
-        '--user_id',
-        default='199351989',
-        help='user id telegram'
-        )
-    args = parser.parse_args()
-    load_dotenv()
-    devman_token = os.environ.get('DEVMAN_TOKEN')
-    telegram_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
-    bot = telegram.Bot(token=telegram_bot_token)
     timestamp = time()
     while True:
         try:
@@ -66,5 +65,20 @@ def main():
 
 
 if __name__ == '__main__':
-    logger.setLevel(logging.ERROR)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-id',
+        '--user_id',
+        default='199351989',
+        help='user id telegram'
+        )
+    args = parser.parse_args()
+    logging.basicConfig(format="%(levelname)s[%(asctime)s]: %(message)s(%(pathname)s: %(funcName)s - line %(lineno)d)")
+    logger.setLevel(logging.DEBUG)
+    load_dotenv()
+    devman_token = os.environ.get('DEVMAN_TOKEN')
+    telegram_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
+    bot = telegram.Bot(token=telegram_bot_token)
+    handler = MyLogsHandler()
+    logger.addHandler(handler)
     main()
